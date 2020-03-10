9a1193c766617cbe40469a4a4df213ae9327fd2e
Alan Ricardo Rocha <alanricardorocha@gmail.com>
2020-03-10 00:12:01 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/TaxaJurosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api1.Controllers
+{
+    public class TaxaJurosController : ControllerBase
+    {
+
+        [HttpGet("taxaJuros")]
+        public decimal TaxaJuros()
+        {
+            return 0.01M;
+        }
+    }
+}
